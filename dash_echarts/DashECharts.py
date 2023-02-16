a2ec04bdce7c74c0d33f24bdec5f762bbaeb19de
Victor Tolosana <victor.tolosana@centralesupelec.fr>
2023-02-16 17:23:10 +0100

    Test2.

--- dash_echarts/DashECharts.py
+++ dash_echarts/DashECharts.py
@@ -18,8 +18,6 @@ Keyword arguments:
 
 - click_data (dict; optional)
 
-- zoom_data (dict; optional)
-
 - event (dict; optional)
 
 - fun_effects (list; optional)
@@ -50,21 +48,22 @@ Keyword arguments:
 
 - selected_data (dict; optional)
 
-- style (dict; optional)"""
+- style (dict; optional)
+
+- zoom_data (dict; optional)"""
+    _children_props = []
+    _base_nodes = ['children']
+    _namespace = 'dash_echarts'
+    _type = 'DashECharts'
     @_explicitize_args
     def __init__(self, resize_id=Component.UNDEFINED, reset_id=Component.UNDEFINED, n_clicks=Component.UNDEFINED, n_clicks_timestamp=Component.UNDEFINED, click_data=Component.UNDEFINED, zoom_data=Component.UNDEFINED, selected_data=Component.UNDEFINED, brush_data=Component.UNDEFINED, style=Component.UNDEFINED, event=Component.UNDEFINED, option=Component.UNDEFINED, maps=Component.UNDEFINED, funs=Component.UNDEFINED, fun_keys=Component.UNDEFINED, fun_values=Component.UNDEFINED, fun_paths=Component.UNDEFINED, fun_effects=Component.UNDEFINED, fun_prepares=Component.UNDEFINED, mapbox_token=Component.UNDEFINED, bmap_token=Component.UNDEFINED, id=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['id', 'bmap_token', 'brush_data', 'click_data', 'zoom_data', 'event', 'fun_effects', 'fun_keys', 'fun_paths', 'fun_prepares', 'fun_values', 'funs', 'mapbox_token', 'maps', 'n_clicks', 'n_clicks_timestamp', 'option', 'reset_id', 'resize_id', 'selected_data', 'style']
-        self._type = 'DashECharts'
-        self._namespace = 'dash_echarts'
+        self._prop_names = ['id', 'bmap_token', 'brush_data', 'click_data', 'event', 'fun_effects', 'fun_keys', 'fun_paths', 'fun_prepares', 'fun_values', 'funs', 'mapbox_token', 'maps', 'n_clicks', 'n_clicks_timestamp', 'option', 'reset_id', 'resize_id', 'selected_data', 'style', 'zoom_data']
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['id', 'bmap_token', 'brush_data', 'click_data', 'zoom_data', 'event', 'fun_effects', 'fun_keys', 'fun_paths', 'fun_prepares', 'fun_values', 'funs', 'mapbox_token', 'maps', 'n_clicks', 'n_clicks_timestamp', 'option', 'reset_id', 'resize_id', 'selected_data', 'style']
+        self.available_properties = ['id', 'bmap_token', 'brush_data', 'click_data', 'event', 'fun_effects', 'fun_keys', 'fun_paths', 'fun_prepares', 'fun_values', 'funs', 'mapbox_token', 'maps', 'n_clicks', 'n_clicks_timestamp', 'option', 'reset_id', 'resize_id', 'selected_data', 'style', 'zoom_data']
         self.available_wildcard_properties =            []
         _explicit_args = kwargs.pop('_explicit_args')
         _locals = locals()
-        _locals.update(kwargs)  # For wildcard attrs
-        args = {k: _locals[k] for k in _explicit_args if k != 'children'}
-        for k in []:
-            if k not in args:
-                raise TypeError(
-                    'Required argument `' + k + '` was not specified.')
+        _locals.update(kwargs)  # For wildcard attrs and excess named props
+        args = {k: _locals[k] for k in _explicit_args}
+
         super(DashECharts, self).__init__(**args)
--- src/dashecharts.jl
+++ src/dashecharts.jl
@@ -5,7 +5,7 @@ using Dash
 const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
 const version = "0.0.12.5"
 
-include("jl/dashecharts.jl")
+include("jl/''_dashecharts.jl")
 
 function __init__()
     DashBase.register_package(
