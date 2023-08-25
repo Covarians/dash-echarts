af47c58acbff282f72851597052ca44e696f8c8f
Victor Tolosana <victor.tolosana@covarians.com>
2023-08-25 15:29:52 +0200

    Added !ramda.isEmpty(chart).

--- src/lib/components/DashECharts.react.js
+++ src/lib/components/DashECharts.react.js
@@ -227,22 +227,24 @@ function DashECharts(props) {
     // useEffect on empty array : will only run after the initial render (twice in debug).
 
     useEffect(() => {
-        // Make area around bars clickable, see https://stackoverflow.com/questions/64643683/how-to-make-space-around-bar-clickable-in-echarts-bar-chart.
-        if (enable_zr_click_event) {
-            chart.getZr().on('click', params => {
-                var pointInPixel = [params.offsetX, params.offsetY];
-                var pointInGrid = chart.convertFromPixel('grid', pointInPixel);
-                var xAxis = chart.getModel().get('xAxis');
-                if (xAxis && xAxis.length > 0) {
-                    var xAxisVal = xAxis[0];
-                    if (xAxis.data.length > pointInGrid[0]) {
-                        // console.log(category);
-                        setProps(
-                            get_category = xAxisVal[pointInGrid[0]]
-                        )
+        if (!ramda.isEmpty(chart)) {
+            // Make area around bars clickable, see https://stackoverflow.com/questions/64643683/how-to-make-space-around-bar-clickable-in-echarts-bar-chart.
+            if (enable_zr_click_event) {
+                chart.getZr().on('click', params => {
+                    var pointInPixel = [params.offsetX, params.offsetY];
+                    var pointInGrid = chart.convertFromPixel('grid', pointInPixel);
+                    var xAxis = chart.getModel().get('xAxis');
+                    if (xAxis && xAxis.length > 0) {
+                        var xAxisVal = xAxis[0];
+                        if (xAxis.data.length > pointInGrid[0]) {
+                            // console.log(category);
+                            setProps(
+                                get_category = xAxisVal[pointInGrid[0]]
+                            )
+                        }
                     }
-                }
-            });
+                });
+            }
         }
     }, [enable_zr_click_event])
 
